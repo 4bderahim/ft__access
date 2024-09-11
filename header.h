2c9b35e25d58783423e792d96dc24318df057f5b
4bderahim <abderahimelkridah@gmail.com>
2024-09-11 18:10:36 +0100

    MyOwnACCESS

--- header.h
+++ header.h
@@ -1,13 +1,10 @@
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 #define FF_OK  0
 #define XX_OK  S_IXGRP           
 #define WW_OK  S_IWGRP          
-#define RR_OK  S_IRGRP          
+#define RR_OK  S_IRGRP   
